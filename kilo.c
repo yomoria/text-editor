c4862a1894f10fce2cf85ffe52b5b74804ac33b5
Yomori <yomoriachiza312@gmail.com>
2025-11-24 16:19:19 +0900

    Vertical & Horizontal Scrolling

--- kilo.c
+++ kilo.c
@@ -49,6 +49,8 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+  int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   struct termios orig_termios; 
@@ -221,10 +223,27 @@ void aBufferFree(struct abuffer *ab) {
 }
 
 /*** output ***/
+void editorScroll() {
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
+}
+
 void editorDrawRows(struct abuffer *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if(y >= E.numrows) {
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -241,9 +260,10 @@ void editorDrawRows(struct abuffer *ab) {
         aBufferAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      aBufferAppend(ab, E.row[y].chars, len);
+      aBufferAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
     aBufferAppend(ab, "\x1b[K", 3);
@@ -254,6 +274,8 @@ void editorDrawRows(struct abuffer *ab) {
 }
 
 void editorRefreshScreen() {
+  editorScroll();
+  
   struct abuffer ab = ABUFFER_INIT;
 
   aBufferAppend(&ab, "\x1b[?25l", 6);
@@ -262,7 +284,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
   aBufferAppend(&ab, buf, strlen(buf));
 
   aBufferAppend(&ab, "\x1b[?25h", 6);
@@ -274,21 +296,23 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_UP:
-      if(E.cy != 0)
+      if (E.cy != 0)
         E.cy--;
       break;
     case ARROW_LEFT:
-      if(E.cx != 0)
+      if (E.cx != 0)
         E.cx--;
       break;
     case ARROW_DOWN:
-      if(E.cy != E.screenrows - 1)
+      if (E.cy < E.numrows - 1)
         E.cy++;
       break;
     case ARROW_RIGHT:
-      if(E.cx != E.screencols - 1)
+      if (row && E.cx < row->size)
         E.cx++;
       break;
   }
@@ -343,6 +367,8 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
